a9cb7346d2213c6993712d9b5f0bd94014cd0728
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2025-10-22 23:58:07 -0600

    T

--- index.js
+++ index.js
@@ -19,22 +19,43 @@ const collectUser = (req, res) => {
 
   const user = data.find((e) => e._id === param);
 
+  res.send({
+    status: true,
+    id: param,
+    user,
+  });
+
   if (!user) {
     return res.status(404).send({
       status: false,
       message: `Usuario con id ${param} no encontrado`,
     });
   }
+};
 
-  res.send({
-    status: true,
-    id: param,
-    user,
-  });
+const queryUser = (req, res) => {
+  const query = req.query;
+  let resultados = data;
+
+  for (const key in query) {
+    resultados = resultados.filter(
+      (item) => String(item[key]) === String(query[key])
+    );
+  }
+
+    if (!query) {
+    return res.status(404).send({
+      status: false,
+      message: `No se encontro nada con ${query}`,
+    });
+  }
+
+  res.send({ finded: true, data: resultados });
 };
 
 app.get("/cuentas", collectUser);
 app.get("/cuentas/:param", collectUser);
+app.get("/cuentas/query", queryUser);
 
 app.listen(PORT, () => {
   console.log(`Estamos al aire en http://localhost:${PORT}`);
